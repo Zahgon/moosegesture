aa9171c1a989e362ee96d5fb045c0b5ab8518ecf
antrikshsinghint17-dotcom <antriksh.singhint17@ethara.ai>
2026-04-28 12:29:18 +0530

    Commit 0

--- moosegesture/__init__.py
+++ moosegesture/__init__.py
@@ -75,7 +75,7 @@ def getGesture(points):
     The `points` parameter is a list of (x, y) tuples of points that make up
     the user's mouse gesture.
     """
-    return _identifyStrokes(points)[0]
+    pass
 
 
 def getSegments(points):
@@ -83,7 +83,7 @@ def getSegments(points):
     Returns a list of tuples of integers. The tuples are the start and end
     indexes of the points that make up a consistent stroke.
     """
-    return _identifyStrokes(points)[1]
+    pass
 
 
 def getGestureAndSegments(points):
@@ -92,8 +92,7 @@ def getGestureAndSegments(points):
     integer, and the second item is a tuple of integers for the start and end
     indexes of the points that make up the stroke.
     """
-    strokes, strokeSegments = _identifyStrokes(points)
-    return list(zip(strokes, strokeSegments))
+    pass
 
 
 def findClosestMatchingGesture(strokes, gestureList, maxDifference=None):
@@ -102,22 +101,7 @@ def findClosestMatchingGesture(strokes, gestureList, maxDifference=None):
     `strokes`. The `maxDifference` is how many differences there can be and still
     be considered a match.
     """
-    if len(gestureList) == 0:
-        return None
-
-    #gestureList = [list(frozenset(tuple(gesture))) for gesture in gestureList] # make a unique list
-    gestureList = frozenset([tuple(gesture) for gesture in gestureList])
-    distances = {}
-    for g in gestureList:
-        levDist = levenshteinDistance(strokes, g)
-        if maxDifference is None or levDist <= maxDifference:
-            distances.setdefault(levDist, [])
-            distances[levDist].append(g)
-
-    if not distances:
-        return None # No matching gestures are within the tolerance of maxDifference.
-
-    return tuple(distances[min(distances.keys())])
+    pass
 
 
 def levenshteinDistance(s1, s2):
@@ -135,121 +119,22 @@ def levenshteinDistance(s1, s2):
     is no way to do it with fewer than three edits:
       kitten -> sitten -> sittin -> sitting
     """
-    singleLetterMapping = {DOWNLEFT: '1', DOWN:'2', DOWNRIGHT:'3',
-                           LEFT:'4', RIGHT:'6',
-                           UPLEFT:'7', UP:'8', UPRIGHT:'9'}
-
-    len1 = len([singleLetterMapping[letter] for letter in s1])
-    len2 = len([singleLetterMapping[letter] for letter in s2])
-
-    matrix = list(range(len1 + 1)) * (len2 + 1)
-    for i in range(len2 + 1):
-        matrix[i] = list(range(i, i + len1 + 1))
-    for i in range(len2):
-        for j in range(len1):
-            if s1[j] == s2[i]:
-                matrix[i+1][j+1] = min(matrix[i+1][j] + 1, matrix[i][j+1] + 1, matrix[i][j])
-            else:
-                matrix[i+1][j+1] = min(matrix[i+1][j] + 1, matrix[i][j+1] + 1, matrix[i][j] + 1)
-    return matrix[len2][len1]
+    pass
 
 
 def _identifyStrokes(points):
-    strokes = []
-    strokeSegments = []
-
-    # calculate lengths between each sequential points
-    distances = []
-    for i in range(len(points)-1):
-        distances.append( _distance(points[i], points[i+1]) )
-
-    # keeps getting points until we go past the min. segment length
-    #startSegPoint = 0
-    #while startSegPoint < len(points)-1:
-    for startSegPoint in range(len(points)-1):
-        segmentDist = 0
-        curDir = None
-        consistent = True
-        direction = None
-        for curSegPoint in range(startSegPoint, len(points)-1):
-            segmentDist += distances[curSegPoint]
-            if segmentDist >= _MIN_STROKE_LEN:
-                # check if all points are going the same direction.
-                for i in range(startSegPoint, curSegPoint):
-                    direction = _getDirection(points[i], points[i+1])
-                    if curDir is None:
-                        curDir = direction
-                    elif direction != curDir:
-                        consistent = False
-                        break
-                break
-        if not consistent:
-            continue
-        elif (direction is not None and ( (not len(strokes)) or (len(strokes) and strokes[-1] != direction) )):
-            strokes.append(direction)
-            strokeSegments.append( [startSegPoint, curSegPoint] )
-        elif len(strokeSegments):
-            # update and lengthen the latest stroke since this stroke is being lengthened.
-            strokeSegments[-1][1] = curSegPoint
-    return strokes, strokeSegments
+    pass
 
 def _getDirection(coord1, coord2):
     """
     Return the direction the line formed by the (x, y)
     points in `coord1` and `coord2`.
     """
-    x1, y1 = coord1
-    x2, y2 = coord2
-
-    if x1 == x2 and y1 == y2:
-        return None # two coordinates are the same.
-    elif x1 == x2 and y1 > y2:
-        return UP
-    elif x1 == x2 and y1 < y2:
-        return DOWN
-    elif x1 > x2 and y1 == y2:
-        return LEFT
-    elif x1 < x2 and y1 == y2:
-        return RIGHT
-
-    slope = float(y2 - y1) / float(x2 - x1)
-
-    # Figure out which quadrant the line is going in, and then
-    # determine the closest direction by calculating the slope
-    if x2 > x1 and y2 < y1: # up right quadrant
-        if slope > -0.4142:
-            return RIGHT # slope is between 0 and 22.5 degrees
-        elif slope < -2.4142:
-            return UP # slope is between 67.5 and 90 degrees
-        else:
-            return UPRIGHT # slope is between 22.5 and 67.5 degrees
-    elif x2 > x1 and y2 > y1: # down right quadrant
-        if slope > 2.4142:
-            return DOWN
-        elif slope < 0.4142:
-            return RIGHT
-        else:
-            return DOWNRIGHT
-    elif x2 < x1 and y2 < y1: # up left quadrant
-        if slope < 0.4142:
-            return LEFT
-        elif slope > 2.4142:
-            return UP
-        else:
-            return UPLEFT
-    elif x2 < x1 and y2 > y1: # down left quadrant
-        if slope < -2.4142:
-            return DOWN
-        elif slope > -0.4142:
-            return LEFT
-        else:
-            return DOWNLEFT
+    pass
 
 def _distance(coord1, coord2):
     """
     Return the distance between two points, `coord1` and `coord2`. These
     parameters are assumed to be (x, y) tuples.
     """
-    xdist = coord1[0] - coord2[0]
-    ydist = coord1[1] - coord2[1]
-    return sqrt(xdist*xdist + ydist*ydist)
+    pass
